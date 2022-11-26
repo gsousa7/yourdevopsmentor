efe8064c6fb634f06cd513da08cc427ec8e88aab
gsousa7 <goncalodcsousa@gmail.com>
2022-11-26 22:10:32 +0000

    Added count and length

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,6 @@
 locals {
-    public_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
-    private_cidr = ["10.0.10.0/24", "10.0.20.0/24"]
+    public_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+    private_cidr = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24", "10.0.40.0/24"]
 }
 
 resource "aws_vpc" "vpc_example" {
@@ -11,7 +11,7 @@ resource "aws_vpc" "vpc_example" {
 }
 
 resource "aws_subnet" "public_subnet" {
-    count = 2
+    count = 4
 
     vpc_id = aws_vpc.vpc_example.id
     cidr_block = local.public_cidr[count.index]
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-    count = 2
+    count = length(local.private_cidr)
 
     vpc_id = aws_vpc.vpc_example.id
     cidr_block = local.private_cidr[count.index]
@@ -38,7 +38,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_eip" "loadbalancer" {
-    count = 2
+    count = length(local.public_cidr)
     
     vpc = true
     tags = {
@@ -47,7 +47,7 @@ resource "aws_eip" "loadbalancer" {
 }
 
 resource "aws_nat_gateway" "nat" {
-    count = 2
+    count = length(local.public_cidr)
 
     allocation_id = aws_eip.loadbalancer[count.index].id
     subnet_id = aws_subnet.public_subnet[count.index].id
@@ -69,7 +69,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table" "private_route_table" {
-    count = 2
+    count = length(local.private_cidr)
     
     vpc_id = aws_vpc.vpc_example.id
 
@@ -83,14 +83,14 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route_table_association" "public_route_association" {
-    count = 2
+    count = length(local.public_cidr)
 
     subnet_id = aws_subnet.public_subnet[count.index].id
     route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "private_route_association" {
-    count = 2
+    count = length(local.private_cidr)
 
     subnet_id = aws_subnet.private_subnet[count.index].id
     route_table_id = aws_route_table.private_route_table[count.index].id
